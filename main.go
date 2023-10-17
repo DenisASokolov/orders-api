fb934d4f49db6c4fb56090f9f6ceec2121cde373
DenisASokolov <denissokolovbusiness2020@gmail.com>
2023-10-17 21:28:59 +0500

    redis db integration

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/DenisASokolov/orders-api/application"
 )
 
 func main() {
 	app := application.New()
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	err := app.Start(ctx)
 	if err != nil {
-		fmt.Println("failed to start server: ", err)
+		fmt.Println("(main.go) failed to start server: ", err)
 	}
 }
